e9d83a9337145ae11097159c0eb2810ce34c1f23
Ayman <ayman.ben.hajjaj@efrei.net>
2021-09-25 22:02:53 +0200

    0.1

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+var collapse = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < collapse.length; i++) {
+    collapse[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+} // Code gentiment prêté par https://www.w3schools.com/howto/howto_js_collapsible.asp
+
+function addItem(game_name, add_btn_num, del_btn_num, categorie) {
+    var ul = document.getElementById(categorie);
+    var candidate = game_name;
+    var li = document.createElement("li");
+    li.setAttribute('id', candidate);
+    li.appendChild(document.createTextNode(candidate));
+    ul.appendChild(li);
+
+    document.getElementById(add_btn_num).disabled = true;
+    document.getElementById(add_btn_num).style.visibility = 'hidden'
+    document.getElementById(del_btn_num).disabled = false;
+    document.getElementById(del_btn_num).style.visibility = 'visible'
+}
+
+
+function removeItem(game_name, add_btn_num, del_btn_num, categorie) {
+    var ul = document.getElementById(categorie);
+    var candidate = game_name;
+    var item = document.getElementById(game_name);
+    ul.removeChild(item);
+
+    document.getElementById(add_btn_num).disabled = false;
+    document.getElementById(add_btn_num).style.visibility = 'visible'
+    document.getElementById(del_btn_num).disabled = true;
+    document.getElementById(del_btn_num).style.visibility = 'hidden'
+}
+
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
