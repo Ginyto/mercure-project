cc2ff9da54175c94a5465d8fe2140413c5574688
Ginyto <agianlucca@gmail.com>
2021-10-24 22:02:25 +0200

    Premiers tests

--- jarvis.js
+++ jarvis.js
@@ -1,9 +1,21 @@
 const Discord = require('discord.js')
-const Client = new Discord.Client({ intents: [] })
 const token = "OTAxODgzMDkxOTU2NjkwOTc1.YXWWFQ.-MRIWawlur8I4_CXvDtIokzkAVE";
+
+const Client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES
+] })
+
 
 Client.on("ready", () => {
     console.log("Link Start")
 })
 
 Client.login(token);
+
+
+Client.on("messageCreate", (message) => {
+    message.reply("Jarvis listening")
+})
