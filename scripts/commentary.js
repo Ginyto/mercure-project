6b8a7c7e96fcd6f0074ff4c79c90dce090c12fbe
Ginyto <agianlucca@gmail.com>
2021-12-29 14:46:00 +0100

    tout est pret pour la partie bdd

--- scripts/commentary.js
+++ scripts/commentary.js
@@ -1,8 +1,17 @@
 console.log("Hello there")
 
 var bdd = require('../bdd.json')
+var fs = require('fs')
 
 console.log(bdd)
+
+bdd.ginyto[0] = "Fanny"
+
+console.log(bdd)
+
+
+
+fs.writeFileSync("bdd.json", JSON.stringify(bdd))
 
 
 
