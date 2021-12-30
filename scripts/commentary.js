fc7e933b0bc0d5279c425076fb48f87d78e43f4c
Ginyto <agianlucca@gmail.com>
2021-12-30 15:29:08 +0100

    fonctionnement done all clear

--- scripts/commentary.js
+++ scripts/commentary.js
@@ -1,3 +1,33 @@
+const dico = {
+    
+    montreal: ["Gianlucca", "Fanny"],
+    cracovie: [],
+    malaisie: [],
+    usa: [],
+    vancouver: [],
+    sot: [],
 
 
+    showcom() {
+        console.log(this.montreal)
+    },
 
+    addnewcom(tab) {
+        tab.push(document.getElementById("comment").value)
+        this.showcom()
+        localStorage.setItem("dico", JSON.stringify(this))
+        
+    },
+
+    test(x) {
+        if (x == 0) {
+            localStorage.clear()
+            alert("localstorage vide")
+        }
+        
+        x = JSON.parse(localStorage.getItem("dico"));
+        console.log("voici le cloud de : "+x.montreal)
+    }
+}
+
+console.log("voici la source local de : "+dico.montreal)
