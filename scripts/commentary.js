54921db9a4defebb3fe1cdb50e2275cda33394cc
Ginyto <agianlucca@gmail.com>
2021-12-30 13:52:37 +0100

    debut de l'objet commentary

--- scripts/commentary.js
+++ scripts/commentary.js
@@ -1,30 +1,3 @@
-var bdd = require('../bdd.json')
-var fs = require('fs')
 
-
-function majbdd() {
-    bdd = json
-    fs.writeFileSync("bdd.json", bdd)
-}
-
-
-function getvalue() {
-    var comment = document.getElementById("comment")
-    return comment.value
-}
-
-function test() {
-    x = JSON.stringify(bdd)
-    addcomment(x)
-}
-
-function addcomment(param) {
-    var espace = document.getElementById("espace")
-    var com = document.createElement("p")
-    com.id = "com"
-    com.textContent = param 
-    espace.appendChild(com)
-
-}
 
 
